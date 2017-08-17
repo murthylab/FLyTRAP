f9d88850123b0b74d2e63ce6dbba80d54a10d6e5
Jan Clemens <clemensjan@googlemail.com>
2017-08-17 14:28:12 -0400

    typo in path

--- video_postProcess.m
+++ video_postProcess.m
@@ -283,7 +283,7 @@ for fil = 1:length(fileName)
       end
       %%
       fprintf('saving to %s\n', fullfile('../../res/', [fileNam(1:end-5) '_spd']) )
-      save(fullfile([resFolder 'res/', [fileNam(1:end-5) '_spd']), ...
+      save(fullfile(resFolder, 'res/', [fileNam(1:end-5) '_spd']), ...
          'spdF', 'posF', 'oriF', 'rDat', 'flyID', 'stiID', 'modID', 'newFs', 'resOns', 'LEDerror', 'chamberSizePX', 'chamberSizeMM', 'FPS')
       %%
       %video_cleanup()
